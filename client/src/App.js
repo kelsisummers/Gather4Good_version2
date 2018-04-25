3c8d4280615a8cd241db9719e611921b133bc9fb
gjb117 <gregbrodzik@gmail.com>
2018-04-24 23:11:33 -0600

    fixing packages and creating setup for testing purposes

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,20 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import CreateEvent from "./pages/CreateEvent";
+import TestPage from "./pages/TestPage";
 
 class App extends Component {
   render() {
     return (
-      <CreateEvent />
+      <Router>
+        <div style={{height: "100%"}}>
+            <h1> G4G </h1>
+            <Switch>
+              <Route exact path="/" component={TestPage} />
+              <Route exact path="/create" component={CreateEvent} />
+            </Switch>
+        </div>
+      </Router>
     );
   }
 }
