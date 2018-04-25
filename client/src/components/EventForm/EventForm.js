3f4b484feb94c016275a2ec2bde87c873f59b16a
gjb117 <gregbrodzik@gmail.com>
2018-04-25 04:32:53 -0600

    start to add form comp

--- client/src/components/EventForm/EventForm.js
+++ client/src/components/EventForm/EventForm.js
@@ -1,10 +1,27 @@
 import React, {Component} from "react";
 import "./EventForm.css";
 
+
 class EventForm extends Component {
   render() {
       return (
-        </div>Hello world</div>
+      <div className="component-wrapper">
+        <div className="form-head">
+          <h2>Create an Event</h2>
+        </div>
+        <div className="form-container">
+          <form>
+            <h3>Step 1: Tell Us About Your Event </h3>
+            <div className="form-step-wrapper">
+  						<input
+                type="text"
+                name="eventName"
+                placeholder="Event Name"
+              />
+					  </div>
+          </form>
+        </div>
+      </div>
     );
   }
 }
--- client/src/pages/CreateEvent.js
+++ client/src/pages/CreateEvent.js
@@ -1,9 +1,17 @@
 import React, {Component} from "react";
+import { Grid, Row, Col } from 'react-bootstrap';
+import EventForm from "../components/EventForm"
 
 class CreateEvent extends Component {
   render() {
       return (
-       <div>This will be the create event page </div>
+        <Grid>
+          <Row>
+            <Col xs={12}>
+              <EventForm/>
+            </Col>
+          </Row>
+        </Grid>
     );
   }
 }
