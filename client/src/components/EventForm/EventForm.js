cf03f1dcf6d0012c2f5bd05d6591d6a8bf4b4656
gjb117 <gregbrodzik@gmail.com>
2018-04-24 22:58:48 -0600

    deleting modules

---
+++ client/src/components/EventForm/EventForm.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import "./EventForm.css";
+
+class EventForm extends Component {
+  render() {
+      return (
+        </div>Hello world</div>
+    );
+  }
+}
+
+export default EventForm;
---
+++ client/src/pages/CreateEvent.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+class CreateEvent extends Component {
+  render() {
+      return (
+       <div>This will be the create event page </div>
+    );
+  }
+}
+
+export default CreateEvent;
