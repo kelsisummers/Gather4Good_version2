c29f29d46e9a5bac918a48f8e5b74150470b0b9b
gjb117 <gregbrodzik@gmail.com>
2018-04-26 00:43:02 -0600

    testing date and time modification for db storage

--- client/src/components/EventForm/EventForm.js
+++ client/src/components/EventForm/EventForm.js
@@ -13,28 +13,56 @@ class EventForm extends Component {
 
   state = {
     date: moment(),
-    eventTime: moment(),
+    time: moment(),
     focused: false,
-    format: 'h:mm a',
-    now: moment().hour(0).minute(0)
+    causeType: "",
+    eventName: "",
+    imgUrl: "",
+    eventDescription: "",
+    streetAddress: "",
+    city: "",
+    USstate: "",
+    zipcode: ""
   };
 
-  onTimeChange = (value) => {
+  //Handles change from timepicker, date change handle in-line
+  handleTimeChange = (value) => {
+    console.log(value._d.getTime());
+    console.log(value._d.toTimeString());
+    console.log(value._d.toDateString());
     console.log(value && value.format('h:mm a'));
-    this.setState({eventTime: value}, () => {
-      console.log("eventime state");
-      console.log(this.state.eventTime);
+    this.setState({time: value}, () => {
+      console.log("Time state");
+      console.log(this.state.time);
     });
   }
 
-  onDateChange = (event) => {
-    console.log(event)
+  handleInputChange = (event) =>  {
+    const { name, value } = event.target;
+    console.log("Name: " + name);
+    console.log("Value: " + value);
+
+    this.setState({
+      [name]: value
+    });
   }
 
-  onChange = date => this.setState({ date })
+  handleDateChange = (date) => {
+    const ISOdate = date._d.toISOString()
+    console.log("CHANGED DATE");
+    console.log(date);
+    console.log(date._d);
+    console.log(ISOdate);
+    this.setState({date}, () => {
+      console.log("Updated date state in CB")
+      console.log(this.state.date);
+    });
+  }
 
   render() {
     console.log(this.state.date);
+    console.log(this.props);
+    console.log(this.props.focused);
 
       return (
       <div className="component-wrapper">
@@ -51,113 +79,124 @@ class EventForm extends Component {
                 type="text"
                 name="eventName"
                 placeholder="Event Name"
+                value={this.state.eventName}
+                onChange={this.handleInputChange}
               />
               <textarea
                 autoComplete="random"
                 rows="2"
                 name="eventDescription"
-                placeholder="Description of your event">
+                placeholder="Description of your event"
+                value={this.state.eventDescription}
+                onChange={this.handleInputChange}>
               </textarea>
-					  </div>
+  				  </div>
 
-          <h3>{`Step 2: What's Your Event's Cause?`}</h3>
-          <div className="form-step-wrapper">
-            <select
-              defaultValue="Choose Your Cause"
-              type="text"
-              name="causeType"
-              placeholder="Event Name">
-                <option value="" disabled={true} hidden={true}> Choose Your Cause</option>
-                <option value="Gender Equality">Gender Equality</option>
-                <option value="LGBTQ">LGBTQ</option>
-                <option value="Environment">Environment</option>
-                <option value="Racial Equality">Racial Equality</option>
-                <option value="Animal Rights">Animal Rights</option>
-                <option value="Education">Education</option>
-                <option value="Human Rights">Human Rights</option>
-                <option value="Public Health">Public Health</option>
-                <option value="Economic Equality ">Economic Equality</option>
-            </select>
-          </div>
+            <h3>{`Step 2: What's Your Event's Cause?`}</h3>
+            <div className="form-step-wrapper">
+              <select
+                defaultValue="Choose Your Cause"
+                type="text"
+                name="causeType"
+                placeholder="Event Name"
+                onChange={this.handleInputChange}>
+                  <option value="Choose Your Cause" disabled={true} hidden={true}> Choose Your Cause</option>
+                  <option value="Gender Equality">Gender Equality</option>
+                  <option value="LGBTQ">LGBTQ</option>
+                  <option value="Environment">Environment</option>
+                  <option value="Racial Equality">Racial Equality</option>
+                  <option value="Animal Rights">Animal Rights</option>
+                  <option value="Education">Education</option>
+                  <option value="Human Rights">Human Rights</option>
+                  <option value="Public Health">Public Health</option>
+                  <option value="Economic Equality ">Economic Equality</option>
+              </select>
+            </div>
 
-          <h3>Step 3: Upload An Image (Optional)</h3>
-          <div className="form-step-wrapper">
-            <input
-              type="text"
-              name="imgUrl"
-              placeholder="Image URL"
-            />
-          </div>
+            <h3>Step 3: Upload An Image (Optional)</h3>
+            <div className="form-step-wrapper">
+              <input
+                type="text"
+                name="imgUrl"
+                placeholder="Image URL"
+                value={this.state.imgUrl}
+                onChange={this.handleInputChange}
+              />
+            </div>
 
-          <h3>Step 4: Event Date and Time</h3>
-          <div id="date-time-wrapper" className="form-step-wrapper">
-
+            <h3>Step 4: Event Date and Time</h3>
+            <div id="date-time-wrapper" className="form-step-wrapper">
               <SingleDatePicker
-                date={this.state.date} // momentPropTypes.momentObj or null
-                onDateChange={date => {console.log(date); this.setState({ date })}} // PropTypes.func.isRequired
-                focused={this.state.focused} // PropTypes.bool
-                onFocusChange={({ focused }) => this.setState({ focused })} // PropTypes.func.isRequired
+                date={this.state.date}
+                onDateChange={this.handleDateChange}
+                focused={this.state.focused}
+                onFocusChange={({ focused }) => this.setState({ focused })}
                 numberOfMonths={1}
               />
-
               <TimePicker
+                name="timepicker"
                 showSecond={false}
-                value={this.state.eventTime}
+                value={this.state.time}
                 className="xxx"
-                onChange={this.onTimeChange}
+                onChange={this.handleTimeChange}
                 format={'h:mm a'}
                 use12Hours
                 inputReadOnly
               />
-
-          </div>
-
-          <h3>Step 5: Event Location</h3>
-            <div id="date-time-wrapper" className="form-step-wrapper">
-            <div className="form-step-wrapper">
-              <input
-                autoComplete="random"
-                type="text"
-                name="streetAdress"
-                placeholder="Street Address"
-              />
-              <input
-                autoComplete="random"
-                type="text"
-                name="city"
-                placeholder="City"
-              />
-              <input
-                autoComplete="random"
-                name="state"
-                list="states"
-                type="text"
-                placeholder="Select a state"
-                id="state"
-              />
-              <datalist id="states">
-                <option value="Alabama"/>
-                <option value="Alaska"/>
-                <option value="Arizona"/>
-                <option value="Arkansas"/>
-                <option value="California"/>
-                <option value="Colorado"/>
-                <option value="Connecticut"/>
-              </datalist>
-
-              <input
-                type="text"
-                name="zipcode"
-                placeholder="Zipcode"
-              />
             </div>
 
-          </div>
+            <h3>Step 5: Event Location</h3>
+              <div className="form-step-wrapper">
+                <input
+                  autoComplete="random"
+                  type="text"
+                  name="streetAddress"
+                  placeholder="Street Address"
+                  value={this.state.streetAddress}
+                  onChange={this.handleInputChange}
+                />
+                <input
+                  autoComplete="random"
+                  type="text"
+                  name="city"
+                  placeholder="City"
+                  value={this.state.city}
+                  onChange={this.handleInputChange}
+                />
+                <input
+                  autoComplete="random"
+                  name="USstate"
+                  list="USstates"
+                  type="text"
+                  placeholder="Select a state"
+                  id="USstate"
+                  value={this.state.USstate}
+                  onChange={this.handleInputChange}
+                />
+                <datalist id="USstates">
+                  <option value="Alabama"/>
+                  <option value="Alaska"/>
+                  <option value="Arizona"/>
+                  <option value="Arkansas"/>
+                  <option value="California"/>
+                  <option value="Colorado"/>
+                  <option value="Connecticut"/>
+                </datalist>
+                <input
+                  type="text"
+                  name="zipcode"
+                  placeholder="Zipcode"
+                  value={this.state.zipcode}
+                  onChange={this.handleInputChange}
+                />
+              </div>
 
-          <button
-            type="submit"
-            className="btn btn-success"> Create Event
-          </button>
+            <button
+              type="submit"
+              className="btn btn-success"
+              onClick={this.props.handleFormSubmit}>
+                Create Event
+            </button>
 
           </form>
         </div>
--- client/src/pages/CreateEvent.js
+++ client/src/pages/CreateEvent.js
@@ -1,14 +1,52 @@
 import React, {Component} from "react";
 import { Grid, Row, Col } from 'react-bootstrap';
 import EventForm from "../components/EventForm"
+import moment from "moment";
 
 class CreateEvent extends Component {
+
+  state = {
+    date: moment(),
+    time: moment(),
+    focused: false,
+    causeType: "",
+    eventName: "",
+    imgUrl: "",
+    eventDescription: "",
+    streetAddress: "",
+    city: "",
+    USstate: "",
+    zipcode: ""
+  };
+
+  handleTimeChange = (value) => {
+    console.log(value && value.format('h:mm a'));
+    this.setState({time: value}, () => {
+      console.log("Time state");
+      console.log(this.state.time);
+    });
+  }
+
+  handleInputChange = (event) =>  {
+    const { name, value } = event.target;
+    console.log("Name: " + name);
+    console.log("Value: " + value);
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleFormSubmit = (event) =>  {
+    console.log( )
+  }
+
   render() {
       return (
         <Grid>
           <Row>
             <Col xs={12}>
-              <EventForm />
+              <EventForm {...this.state} handleFormSubmit={this.handleFormSubmit} />
             </Col>
           </Row>
         </Grid>
