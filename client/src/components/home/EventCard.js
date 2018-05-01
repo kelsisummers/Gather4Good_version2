871a8ae372116455c3979533e414bcdb0103d693
Benjamin Cash <cash.benj@gmail.com>
2018-04-30 20:50:32 -0600

    displaying badge if organizer

--- client/src/components/home/EventCard.js
+++ client/src/components/home/EventCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Panel, Button } from 'react-bootstrap';
+import { Panel, Button, Badge } from 'react-bootstrap';
 
 
 export const EventCard = (props) => {
@@ -10,7 +10,7 @@ export const EventCard = (props) => {
             return (
                 <Panel>
                     <h1>{props.data.title}</h1>
-                    <h2>Organizer ID: {organizerId}</h2>
+                    <Badge>Organizer</Badge>
                     
                     <p>{props.data.description}</p>
                     <Button bsStyle="primary" eventId={props.data._id} eventTitle={props.data.title} onClick={props.handleJoinEventButtonClick}>Join Event</Button>
