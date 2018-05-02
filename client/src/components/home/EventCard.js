6d4d71933e7ac2e7946abd0de3dc5a392c779227
Benjamin Cash <cash.benj@gmail.com>
2018-05-01 20:02:00 -0600

    adding functionality to show going badge on events in list on homepage if user is an attendee

--- client/src/components/home/EventCard.js
+++ client/src/components/home/EventCard.js
@@ -4,29 +4,21 @@ import { Panel, Button, Badge } from 'react-bootstrap';
 
 export const EventCard = (props) => {
     
-        let userId = props.userId;
-        let organizerId = props.data.organizer_id;
-        if(userId == organizerId) {
-            return (
-                <Panel>
-                    <h1>{props.data.title}</h1>
-                    <Badge>Organizer</Badge>
-                    
-                    <p>{props.data.description}</p>
-                    <Button bsStyle="primary" eventId={props.data._id} eventTitle={props.data.title} onClick={props.handleJoinEventButtonClick}>Join Event</Button>
-                </Panel>
-            )
+    let userId = props.userId;
+    let organizerId = props.data.organizer_id;
+    let attendees = props.data.attendees;
+    const organizerBadge = (userId == organizerId) ? (<Badge>Organizer</Badge>) : (<div/>);
+    const attendeeBadge = (attendees.includes(userId)) ? (<Badge>Going</Badge>) : (<div/>)
 
-        } else {
-            return (
-                <Panel>
-                    <h1>{props.data.title}</h1>
-                    <p>{props.data.description}</p>
-                    <Button bsStyle="primary" eventId={props.data._id} eventTitle={props.data.title} onClick={props.handleJoinEventButtonClick}>Join Event</Button>
-                </Panel>
-            )
 
-        }
+    return (
+        <Panel>
+            <h1>{props.data.title}</h1>
+            {organizerBadge}
+            {attendeeBadge}
+            <p>{props.data.description}</p>
+            <Button bsStyle="primary" eventId={props.data._id} eventTitle={props.data.title} onClick={props.handleJoinEventButtonClick}>Join Event</Button>
+        </Panel>
+    )
 
-    
 }
