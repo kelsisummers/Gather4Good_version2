d352d38d91d1376ea0aa640c17d78b7586c58bce
Molly Wojcik <mollywojcik@gmail.com>
2018-05-05 12:41:20 -0600

    Filter by 'my events' is working with both organizer and attendee event statuses displaying

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -119,9 +119,14 @@ class Home extends Component {
       })
   }
 
-  myEvents() {
+  myEvents = () => {
     // setState to hold events user is attending or has organized.
     // Need to discuss how to handle this..
+    const userId = this.props.authData.user_id
+    API.getUserEvents(userId)
+      .then((events) => {
+        console.log(events);
+      })
   }
 
   render() {
