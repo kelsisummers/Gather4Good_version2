55a8ee8d2fa3d5a7fcc1003dd0987dbc12f024bd
Molly Wojcik <mollywojcik@gmail.com>
2018-05-07 08:10:19 -0600

    updated button color/border on comments form

--- client/src/components/single-event/Discussion/CommentForm.js
+++ client/src/components/single-event/Discussion/CommentForm.js
@@ -1,14 +1,18 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "#00b9b4",
+  border: "none",
+  borderRadius: "2px"
+}
+
 const CommentForm = (props) => {
     return (
         <div>
             <form onSubmit={props.handleCommentFormSubmit}>
                 <p className='discussion-placeholder'>Type new comment below:</p>
                 <input value={props.commentFormInputValue} id="input" placeholder='Someone want to carpool?' onChange={props.handleCommentInputChange}></input>
-                <button className='btn btn-success' type="submit">Submit Comment</button>
-
-
+                <button style={style} className='btn btn-success' type="submit">Submit Comment</button>
             </form>
         </div>
     )
